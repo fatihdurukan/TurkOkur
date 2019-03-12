d46315563019dc338aaac2765549f25d87ebd057
Fatih Durukan <fatihdurukan42@gmail.com>
2019-03-12 12:14:36 -0500

    TurkOkur project

---
+++ config.ini.php
@@ -0,0 +1,8 @@
+; <?php exit; ?> DO NOT REMOVE THIS LINE
+[database]
+host                 = localhost
+username             = root
+password             = unuttum42
+dbname               = social3
+port                 = 3306
+adapter              = PDO_MYSQL ; PDO_MYSQL or MYSQLI
